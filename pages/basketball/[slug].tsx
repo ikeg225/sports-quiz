3ba243c8d5f065621b257697d2d2f08af511d838
Ethan Ikegami <ikeg225@berkeley.edu>
2022-05-03 00:42:00 -0700

    blog and contact pages

--- pages/basketball/[slug].tsx
+++ pages/basketball/[slug].tsx
@@ -9,19 +9,21 @@ import { Post } from '../../typings'
 import { getData } from '../api/mongo'
 import { GetStaticProps } from "next"
 import { sanityClient, urlFor } from "../../sanity"
+import { mainModule } from 'process'
 
 interface Props {
     post: Post;
+    blog: any;
     posts: any;
     quizInfo: any;
 }
 
-function Post({ post, posts, quizInfo }: Props) {
+function Post({ post, blog, posts, quizInfo }: Props) {
     return (
         <div className="max-w-7xl mx-auto h-full">
             <div className="md:mx-5 h-full" id="outer-container">
                 <Head>
-                <title>{post.title}</title>
+                <title>{post ? post.title : blog.name}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <link
                     rel="preload"
@@ -33,26 +35,30 @@ function Post({ post, posts, quizInfo }: Props) {
                 <Header />
                 <main id="page-wrap" className="flex flex-row md:mt-10 flex-wrap">
                     <div className="w-full md:w-7/12">
-                        <Image 
-                            className="cursor-pointer"
-                            src={urlFor(post.mainImage).url()}
-                            alt={post.title}
-                            height={675}
-                            width={1200}
-                        />
+                        <div className="text-[0px]">
+                            <Image 
+                                src={urlFor(post ? post.mainImage : blog.image).url()}
+                                alt={post ? post.title : blog.name}
+                                height={675}
+                                width={1200}
+                            />
+                        </div>
                         <div className="m-5 md:m-0">
                             <h1 className="text-2xl my-5 font-header uppercase text-center md:text-left">
-                                {post.title}
+                                {post ? post.title : blog.name}
                             </h1>
-                            <StartQuiz url={post.id} values={quizInfo[post.id]} />
+                            {post && <StartQuiz url={post.id} values={quizInfo[post.id]} />}
                             <PortableText 
                                 className=""
                                 dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
                                 projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-                                content={post.body}
+                                content={post ? post.body : blog.body}
                                 serializers={{
                                     h2: (props: any) => (
                                         <h1 className="text-xl my-3 font-header uppercase" {...props} />
+                                    ),
+                                    h3: (props: any) => (
+                                        <h1 className="text-lg my-3 font-header uppercase" {...props} />
                                     ),
                                     li: ({ children } : any) => (
                                         <li className="list-disc ml-4">{children}</li>
@@ -66,7 +72,7 @@ function Post({ post, posts, quizInfo }: Props) {
                             />
                         </div>
                     </div>
-                    <div className="w-full md:w-5/12 m-5 md:m-0">
+                    <div className="w-full md:w-5/12 mx-5 md:m-0">
                         <SideBar posts={posts} />
                     </div>
                 </main>
@@ -79,7 +85,7 @@ function Post({ post, posts, quizInfo }: Props) {
 export default Post;
 
 export const getStaticPaths = async () => {
-    const query = `*[_type == "post"] {
+    const query = `*[_type == "post" || _type == "blog"] {
         _id,
         slug{
         current
@@ -111,14 +117,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       body
       }`
 
-    const post = await sanityClient.fetch(query, {
+    let post = await sanityClient.fetch(query, {
         slug: params?.slug
     })
 
     if (!post) {
-        return {
-            notFound: true
-        }
+        post = false
     }
 
     const query_posts = `*[_type == "post"] {
@@ -130,11 +134,30 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     
     const posts = await sanityClient.fetch(query_posts);
 
+    const queryBlog = `*[_type == "blog" && slug.current == $slug][0] {
+        _id,
+        slug{
+        current
+      },
+        image,
+        name,
+      body
+      }`
+
+    let blog = await sanityClient.fetch(queryBlog, {
+        slug: params?.slug
+    })
+
+    if (!blog) {
+        blog = false
+    }
+
     const quizInfo = await getData()
 
     return {
         props: {
             post,
+            blog,
             posts,
             quizInfo
         },
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import SideBar from '../components/SideBar'
+import { urlFor } from '../sanity.js'
+import { sanityClient } from '../sanity.js'
 
-export default function Blog() {
+export default function Blog({ blogs, posts } : any) {
     return (
         <div className="max-w-7xl mx-auto h-full">
             <div className="md:mx-5 h-full" id="outer-container">
@@ -18,10 +23,60 @@ export default function Blog() {
                 </Head>
                 <Header />
                 <main id="page-wrap">
-                    <h1>blog</h1>
+                    <div className="flex flex-row pt-5 m-5 md:m-0">
+                        <div className="flex flex-col gap-10 lg:w-8/12">
+                            {blogs.map((blog : any)=> (
+                                <Link key={blog.name} href={`/basketball/${blog.slug.current}`}>
+                                    <div className="flex flex-col md:flex-row ease-in-out duration-100 cursor-pointer bg-white hover:drop-shadow-lg items-center">
+                                        <div className="md:max-w-xs text-[0px]">
+                                            <Image
+                                                src={urlFor(blog.image).url()}
+                                                width={1200}
+                                                height={675}
+                                            />
+                                        </div>
+                                        <div className="p-5 md:p-0 md:pl-5">
+                                            <h1 className="font-header text-black uppercase text-xl">
+                                                {blog.name}
+                                            </h1>
+                                            <p>{blog.body[0].children[0].text.substring(0,125)}...</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))}
+                        </div>
+                        <div className="hidden lg:block lg:w-4/12">
+                            <SideBar posts={posts} />
+                        </div>
+                    </div>
                 </main>
                 <Footer />
             </div>
         </div>
     )
 }
+
+export const getServerSideProps = async () => {
+    const query = `*[_type == "blog"] {
+        name,
+        image,
+        slug,
+        body
+      }`;
+
+      const queryQuiz = `*[_type == "post"] {
+        title,
+        mainImage,
+        slug
+      }`;
+
+    const blogs = await sanityClient.fetch(query);
+    const posts = await sanityClient.fetch(queryQuiz);
+
+    return {
+        props: {
+        blogs,
+        posts
+        },
+    };
+};
