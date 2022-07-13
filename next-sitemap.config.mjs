ef7b0beb9908dd76abbba114d83ba36d5cf6cb9a
Ethan Ikegami <ikeg225@berkeley.edu>
2022-07-13 12:08:49 -0700

    fixed sitemap

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: process.env.SITE_URL || 'https://sportsquiz.org',
+    siteUrl: 'https://sportsquiz.org',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }
